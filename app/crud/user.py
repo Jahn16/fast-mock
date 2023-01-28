5f833b5a1dffead69a2d08000899b0b7fc6f574d
Jahn16 <jpdeoliveiraferreira@gmail.com>
2023-01-28 17:31:05 -0300

    fix: fixed missing email

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,24 +1,24 @@
 from sqlalchemy.orm import Session
 
-from app import schemas
-from app import models
+from app.models.user import User
+from app.schemas.user import UserCreate
 
 
 def get_user(db: Session, user_id: int):
-    return db.query(models.user.User).filter(models.User.id == user_id).first()
+    return db.query(User).filter(User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(models.user.User).filter(models.user.User == email).first()
+    return db.query(User).filter(User.email == email).first()
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.user.User).offset(skip).limit(limit).all()
+    return db.query(User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: UserCreate):
     fake_hashed_password = user.password = "notreallyhashed"
-    db_user = models.user.User(
+    db_user = User(
         email=user.email, hashed_password=fake_hashed_password
     )
     db.add(db_user)
