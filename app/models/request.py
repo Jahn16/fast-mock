e4b6278b9a21c10769781691b24a6ae14b3c92db
Jahn16 <jpdeoliveiraferreira@gmail.com>
2023-01-28 16:31:18 -0300

    feat: added models

---
+++ app/models/request.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    endpoint = Column(String, index=True)
+    response = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="requests")
---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    requests = relationship("Request", back_populates="owner")
