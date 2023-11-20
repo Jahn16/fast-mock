5ca1994659b9d17fedc07359d601e24a9501e784
Jahn16 <jpdeoliveiraferreira@gmail.com>
2023-11-20 16:58:27 -0300

    :loud_sound: Using structlog for logging

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 from app.api.api_v1.api import api_router
 from app.config import settings
+from app.middlewares.logger import LogMiddleware
+from app.utils.logging import setup_logging
 
+setup_logging()
 app = FastAPI(title=settings.PROJECT_NAME)
 
+app.add_middleware(LogMiddleware)
 app.include_router(api_router)
