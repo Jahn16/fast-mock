59fb59e07bf8d1413f23bda42d5d057602d932a6
Jahn16 <jpdeoliveiraferreira@gmail.com>
2023-11-20 18:03:12 -0300

    :card_file_box: Added one to many relationship between url and user

--- app/models/url.py
+++ app/models/url.py
@@ -1,16 +1,17 @@
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 
 from app.database import Base
+
 
 class URL(Base):
     __tablename__ = "urls"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     hostname = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
     requests = relationship("Request", back_populates="url")
-
-
-
+    owner = relationship("User", back_populates="urls")
